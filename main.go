aba2acd1f5f5aa27c28a312165ee0dbe0160b320
Michał Niewczas <michal.niewczas@merapar.com>
2020-10-01 22:58:18 +0200

    rework heartbeats

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/rs/cors"
 )
@@ -26,7 +26,24 @@ type Heartbeat struct {
 	Details   heartbeatDetails
 }
 
-var hbarr []Heartbeat
+//Heartbeats - structure stored by app
+type Heartbeats struct {
+	Model       string
+	TimeStarted string
+	TimeEnded   string
+	LastUpdate  string
+	Details     heartbeatsDetails
+}
+
+type heartbeatsDetails struct {
+	TotalRAM     []int
+	UsedRAM      []int
+	CPU          []float64
+	VideoBitrate []int
+	Iteration    []int
+}
+
+var hbarr map[string]*Heartbeats = make(map[string]*Heartbeats)
 
 func getHeartbeats(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -38,6 +55,47 @@ func getHeartbeats(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
+func setDetails(h Heartbeat, hs *Heartbeats) {
+	hs.Details.TotalRAM = append(hs.Details.TotalRAM, h.Details.TotalRAM)
+	hs.Details.UsedRAM = append(hs.Details.UsedRAM, h.Details.UsedRAM)
+	hs.Details.CPU = append(hs.Details.CPU, h.Details.CPU)
+	hs.Details.VideoBitrate = append(hs.Details.VideoBitrate, h.Details.VideoBitrate)
+	hs.LastUpdate = h.Timestamp
+}
+
+func endHeartbeat(w http.ResponseWriter, r *http.Request) {
+	var h Heartbeat
+	// var hs Heartbeats
+	err := json.NewDecoder(r.Body).Decode(&h)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	var hs = hbarr[h.Model]
+	hs.TimeEnded = h.Timestamp
+	setDetails(h, hs)
+
+	f, err := os.Create("local\\log\\" + h.Model + "_" + h.Timestamp + ".json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	js, err := json.Marshal(hbarr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err2 := f.Write(js)
+
+	if err2 != nil {
+		log.Fatal(err)
+	}
+
+}
 func saveHeartbeat(w http.ResponseWriter, r *http.Request) {
 	var h Heartbeat
 	err := json.NewDecoder(r.Body).Decode(&h)
@@ -45,24 +103,30 @@ func saveHeartbeat(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	var i = 0
-	for range hbarr {
-		if hbarr[i].Model == h.Model {
-			hbarr[i] = h
-			break
-		}
-		i++
+
+	if hbarr[h.Model] != nil && len(hbarr[h.Model].TimeEnded) > 0 {
+		delete(hbarr, h.Model)
 	}
-	if i+1 > len(hbarr) {
-		hbarr = append(hbarr, h)
+
+	if hs, ok := hbarr[h.Model]; ok {
+		setDetails(h, hs)
+		hs.Details.Iteration = append(hs.Details.Iteration, hs.Details.Iteration[len(hs.Details.Iteration)-1]+1)
+	} else {
+		hbarr[h.Model] = &Heartbeats{}
+		var hs = hbarr[h.Model]
+		hs.Model = h.Model
+		hs.TimeStarted = h.Timestamp
+		hs.Details.Iteration = append(hs.Details.Iteration, 0)
+		setDetails(h, hs)
+		hs.LastUpdate = h.Timestamp
 	}
-	fmt.Printf("New heartbeat: %+v\n", h)
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/heartbeat/add", saveHeartbeat)
 	mux.HandleFunc("/heartbeat/get", getHeartbeats)
+	mux.HandleFunc("/heartbeat/end", endHeartbeat)
 	handler := cors.Default().Handler(mux)
 	err := http.ListenAndServe(":3000", handler)
 	log.Fatal(err)
